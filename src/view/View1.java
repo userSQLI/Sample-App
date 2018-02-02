6b1c06494cdd2e3ae75572fdc43afc3b9e23665b
userSQLI <36069588+userSQLI@users.noreply.github.com>
2018-02-02 09:27:12 +0000

    Create View1.java

---
+++ src/view/View1.java
@@ -0,0 +1,17 @@
+public class View1 {
+
+	static int hello;
+
+	private static int add(int a, int b) {
+		return a + b;
+	}
+
+	public static void main(String[] args) {
+
+		String message = "Hello world";
+		System.out.println(message);
+		c = add(a, b);
+		System.out.println(c);
+	}
+
+}
