49fd7d18ccbe8b1b19377fd4c6770acbe95c7f38
moad eddaalous <meddaalous@sqli.com>
2018-02-02 11:39:45 +0000

    div

--- src/view/View1.java
+++ src/view/View1.java
@@ -6,14 +6,24 @@ public class View1 {
 		return a / b;
 	}
 
+    private static int multiply(int a, int b) {
+        return a * b;
+    }
 
-	public static void main(String[] args) {
+    private static int subs(int a, int b) {
+        return a - b;
+    }
 
-        int a;
-		String message = "Hello world";
-		System.out.println(message);
-		c = add(a, b);
-		System.out.println(c);
-	}
+    public static void main(String[] args) {
+
+      String message = "Hello world";
+      System.out.println(message);
+      int a=5;
+      int b=6;
+      System.out.println("Addition: "add(a,b));
+      System.out.println("Multiply: "multiply(a,b));
+      System.out.println("Subs: "subs(a,b));
+
+  }
 
 }
